5ca26053b9f4551f23b53f5b4ea6836a7d05b880
Lucas Cauthen <lucascauthen@gmail.com>
2017-11-29 14:21:19 -0800

    Finally working InsertionPreservingDictionary

---
+++ src/test/java/datastructures/dictionaries/TestInsertionPreservingDictionary.java
@@ -0,0 +1,34 @@
+package datastructures.dictionaries;
+
+import org.junit.Test;
+
+import datastructures.concrete.KVPair;
+import datastructures.concrete.dictionaries.InsertionPreservingDictionary;
+import datastructures.interfaces.IDictionary;
+
+public class TestInsertionPreservingDictionary extends TestChainedHashDictionary{
+	protected <K, V> IDictionary<K, V> newDictionary() {
+        return new InsertionPreservingDictionary<>();
+    }
+	
+	@Test(timeout=SECOND)
+	public void testPreserveInsertionOrder() {
+        IDictionary<String, String> dict = this.newDictionary();
+        dict.put("1", "1");
+        dict.put("2", "2");
+        dict.put("4", "3");
+        dict.put("8", "4");
+        dict.put("16", "5");
+        dict.put("32", "6");
+        dict.put("64", "7");
+
+        int countA = 1;
+        int countB = 1;
+        for(KVPair<String, String> pair : dict) {
+        		assertEquals(String.valueOf(countB), pair.getKey());
+        		assertEquals(String.valueOf(countA), pair.getValue());
+        		countA++;
+        		countB *= 2;
+        }
+    }
+}
